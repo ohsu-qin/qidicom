4e58d87bcb9059a111efe2e00fb30663facb55c5
FredLoney <loneyf@ohsu.edu>
2015-01-14 11:04:47 -0800

    Bump version.

--- qidicom/__init__.py
+++ qidicom/__init__.py
@@ -2,7 +2,7 @@
 This qidicom module contains utilities to read and edit DICOM files.
 """
 
-__version__ = '2.1.4'
+__version__ = '2.1.5'
 """
 The one-based major.minor.patch version.
 The version numbering scheme loosely follows http://semver.org/.
