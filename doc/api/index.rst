a94512efeaeb1f6d2338594b7c4cc2fed235399a
FredLoney <loneyf@ohsu.edu>
2015-02-18 16:37:35 -0800

    Standardize the autodoc directives.

--- doc/api/index.rst
+++ doc/api/index.rst
@@ -7,23 +7,15 @@ API Documentation
 :mod:`hierarchy`
 ----------------
 .. automodule:: qidicom.hierarchy
-    :members:
-    :show-inheritance:
 
 :mod:`meta`
 -----------
 .. automodule:: qidicom.meta
-    :members:
-    :show-inheritance:
 
 :mod:`reader`
 -------------
 .. automodule:: qidicom.reader
-    :members:
-    :show-inheritance:
 
 :mod:`writer`
 -------------
 .. automodule:: qidicom.writer
-    :members:
-    :show-inheritance:
--- qidicom/__init__.py
+++ qidicom/__init__.py
@@ -4,11 +4,7 @@ This qidicom module contains utilities to read and edit DICOM files.
 
 __version__ = '2.2.1'
 """
-The one-based major.minor.patch version.
-The version numbering scheme loosely follows http://semver.org/.
-The major version is incremented when a significant feature
-set is introduced. The minor version is incremented when there
-is a functionality change. The patch version is incremented when
-there is a refactoring or bug fix. All major, minor and patch
-version numbers begin at 1.
+The one-based major.minor.patch version based on the
+`Fast and Loose Versioning <https://gist.github.com/FredLoney/6d946112e0b0f2fc4b57>`_
+scheme. Minor and patch version numbers begin at 1.
 """
