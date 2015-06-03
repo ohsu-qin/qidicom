3468a4f8af95e2eb38b879b7035a7cb25893f85c
FredLoney <loneyf@ohsu.edu>
2015-06-03 10:55:49 -0700

    Add comments.

--- qidicom/writer.py
+++ qidicom/writer.py
@@ -34,12 +34,16 @@ def edit(*in_files, **opts):
     # directory.
     for ds in reader.iter_dicom(*in_files):
         logger(__name__).debug("Editing the DICOM file %s..." % ds.filename)
+        # Delegate the edit.
         yield ds
+        # Make the output file name.
         if isinstance(dest, str):
+            # The dest option is a parent directory.
             _, fname = os.path.split(ds.filename)
             out_file = os.path.join(dest, fname)
         else:
+            # The dest option is a function.
             out_file = dest(ds.filename)
+        # Save the modified dataset.
         ds.save_as(out_file)
         logger(__name__).debug("Saved the edited DICOM file as %s." % out_file)
-    logger(__name__).info("The edited DICOM files were saved in %s." % dest)
